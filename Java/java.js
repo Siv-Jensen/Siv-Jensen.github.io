bcd533e28054a96b0d6fcba08d5c6f17d4d69b95
Siv-Jensen <Froysa0@gmail.com>
2024-10-18 13:24:39 +0200

    java

--- Java/java.js
+++ Java/java.js
@@ -1,8 +1,29 @@
-alert("gå vekk");
+alert("God dag");
 
-console.log("Piss");
+console.log("nah");
 
-let navn = prompt("Skriv navnet ditt")
+let navn = prompt("Hva heter du?")
 console.log(navn);
 
-document.getElementById("utskrift").innerText="Hei, " + navn;
+alert("Hei, " + navn);
+
+let aarstall = prompt("I hvilket år ble du født?")
+console.log(aarstall);
+
+let dato = new Date()
+console.log(dato.getFullYear());
+
+let alder = dato.getFullYear() - aarstall
+console.log(alder);
+
+document.getElementById("utskrift").innerText="Hei, " + navn + ". Du er " + alder + 5 + " år gammel.";
+
+if(alder > 17){
+    document.getElementById("utskrift2").innerText="Det vil si at du er gammel og grå."
+}
+else if(alder = 17){
+    document.getElementById("utskrift2").innerText="Det vil si du er nyfødt."
+}
+else{
+    document.getElementById("utskrift2").innerText="Det vil si du er nyfødt."
+}
