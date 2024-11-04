6a03911954443589ca43960e39571ee0cd64a1ff
Siv-Jensen <Froysa0@gmail.com>
2024-11-04 10:38:07 +0100

    blackjack

---
+++ Java/Java Øving/Blackjack/blackjack.js
@@ -0,0 +1,15 @@
+let startKnapp = document.getElementById("spill");
+startKnapp.addEventListener("trykkStart", "click");
+function delUtKort() {
+
+}
+function trykkStart() {
+    addEventListener("delUtKort")
+};
+
+
+
+let trekkInn = document.getElementById("trekk inn");
+let sto = document.getElementById("stå");
+let trekkSjanse = document.getElementById("ta sjanse");
+let splitt = document.getElementById("splitt kort");
