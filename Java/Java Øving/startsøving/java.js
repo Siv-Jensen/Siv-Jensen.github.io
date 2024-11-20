a91be5b188060ca3214aad3321f39a7576ccd4c3
Siv-Jensen <Froysa0@gmail.com>
2024-11-20 15:48:10 +0100

    lagde input

--- Java/Java Øving/startsøving/java.js
+++ Java/Java Øving/startsøving/java.js
@@ -1,66 +1,43 @@
-// alert("God dag");
+// let listeNavn = ["pulings", "samleie", "koding", "piss"]
 
-// console.log("nah");
-
-// let navn = prompt("Hva heter du?")
-// console.log(navn);
-
-// alert("Hei, " + navn);
-
-// let aarstall = prompt("I hvilket år ble du født?")
-// console.log(aarstall);
-
-// let dato = new Date()
-// console.log(dato.getFullYear());
-
-// let alder = dato.getFullYear() - aarstall
-// console.log(alder);
-
-// document.getElementById("utskrift").innerText="Hei, " + navn + ". Du er " + alder + 5 + " år gammel.";
-
-// if(alder > 17){
-//     document.getElementById("utskrift2").innerText="Det vil si at du er gammel og grå."
-// }
-// else if(alder = 17){
-//     document.getElementById("utskrift2").innerText="Det vil si du er nyfødt."
-// }
-// else{
-//     document.getElementById("utskrift2").innerText="Det vil si du er nyfødt."
-// }
-// let bakgrunnsfarger = prompt("Velg to farger, separert med komma.")
-//     document.querySelector("body").style.backgroundColor = "linear-gradient("+ bakgrunnsfarger +")";
-
-// let bakgrunnsfarge = prompt("Gjett riktig farge.")
-// console.log(bakgrunnsfarge);
-
-// if(bakgrunnsfarge = (0,0,255)){
-//     document.body.backroundColor = green
+// for (let i = 0; i < listeNavn.length; i = i + 1) {
+//     console.log(listeNavn[i])
 // }
 
-// else{
-//     document.body.backgroundColor = red
-// };
+// let listeSpørsmål = ["kor e du?", "kor går du?"];
+// let listeSvar = ["ved deg", "inn i deg", "ut"];
 
-let listeNavn = ["pulings", "samleie", "koding", "piss"]
+// for (let x = 0; x < listeSpørsmål.length; x++) {
+//     console.log("Spørsmål: " + listeSpørsmål[x])
+//     let svar = prompt("Spørsmål: " + listeSpørsmål[x]);
 
-for (let i = 0; i < listeNavn.length; i = i + 1) {
-    console.log(listeNavn[i])
-}
+//     if (svar == listeSvar[x]) {
+//         console.log("yuh");
+//     }
+//     else {
+//         console.log("nah");
+//         console.log("va egt enten '" + listeSvar[x] + "' eller '" + listeSvar[x + 1]+ "'");
+//     }
 
-let listeSpørsmål = ["kor e du?", "kor går du?"];
-let listeSvar = ["ved deg", "inn i deg", "ut"];
+//     console.log("Svar: Enten " + listeSvar[x] + " eller " + listeSvar[x + 1])
+// }
 
-for (let x = 0; x < listeSpørsmål.length; x++) {
-    console.log("Spørsmål: " + listeSpørsmål[x])
-    let svar = prompt("Spørsmål: " + listeSpørsmål[x]);
+let navn = document.getElementById("navn");
+let passord = document.getElementById("passord");
+let farge = document.getElementById("farge");
 
-    if (svar == listeSvar[x]) {
-        console.log("yuh");
-    }
-    else {
-        console.log("nah");
-        console.log("va egt enten '" + listeSvar[x] + "' eller '" + listeSvar[x + 1]+ "'");
-    }
+let knapp = document.getElementById("knapp");
 
-    console.log("Svar: Enten " + listeSvar[x] + " eller " + listeSvar[x + 1])
+knapp.addEventListener("click", send);
+
+function send() {
+    let navnet = navn.value;
+    let passordet = passord.value;
+    let fargen = farge.value;
+
+    console.log("Navn: " + navnet);
+    console.log("Passord: " + passordet);
+    console.log("Valgt farge:" + fargen);
+
+    document.getElementById(kroppenMin).stylesheet.color = (fargen);
 }
