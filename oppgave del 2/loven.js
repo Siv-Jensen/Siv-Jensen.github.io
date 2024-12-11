8f67d50ad596a1c880aed86e5bf8c054fc974ca5
Siv-Jensen <Froysa0@gmail.com>
2024-12-11 15:46:24 +0100

    jobbet på å lage svar på skjemaet

--- oppgave del 2/loven.js
+++ oppgave del 2/loven.js
@@ -1,7 +1,25 @@
 document.getElementById("skjema").addEventListener("submit", fikseSkjema);
 
-fucntion fikseSkjema(event) {
+document.getElementById("svar").addEventListener("submit", fikseSkjema);
 
-    event.preventDefault()
+document.getElementById("svar").style.visibility = "hidden";
 
-};
+function fikseSkjema(event) {
+
+    event.preventDefault();
+
+    let alder = document.querySelector("#tekst").value;
+
+    console.log("Alder: " + alder);
+
+    document.getElementById("svar").style.visibility = "visible";
+
+}
+
+alert("Takk for svaret, her er hva loven sier");
+
+let hovedTekst = document.getElementById("hoved");
+
+if(alder < 17) {
+    print ("Du kan kjøre bil.")
+}
